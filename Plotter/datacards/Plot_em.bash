f930a8877773065cc80c2e1288339cf4472491fd
Aliaksei Raspiareza <rasp@mail.desy.de>
2022-11-16 19:26:00 +0100

    updated datacards scripts

--- Plotter/datacards/Plot_em.bash
+++ Plotter/datacards/Plot_em.bash
@@ -5,10 +5,13 @@ Starting --->
 
 EOF
 
-for era in 2016 2017 2018
+for era in 2018
 do 
-    for category in em_Nbtag0_lowPzeta em_Nbtag0_Pzeta1 em_Nbtag0_Pzeta2 em_Nbtag0_Pzeta3 em_NbtagGe1_lowPzeta em_NbtagGe1_Pzeta1 em_NbtagGe1_Pzeta2 em_NbtagGe1_Pzeta3
+#    for category in em_Nbtag0_lowPzeta em_Nbtag0_Pzeta1 em_Nbtag0_Pzeta2 em_NbtagGe1_lowPzeta em_NbtagGe1_Pzeta1 em_NbtagGe1_Pzeta2
+#    for category in em_Nbtag0_Pzeta1 em_Nbtag0_Pzeta2 em_NbtagGe1_Pzeta1 em_NbtagGe1_Pzeta2
+#    for category in em_cat0 em_cat1
+    for category in em_cat2 em_cat3 em_cat2_Nbtag0 em_cat2_NbtagGe1 em_cat3_Nbtag0 em_cat3_NbtagGe1 
     do 
-	PlotDatacards ${era} ${category} em
+	PlotDatacards ${era} ${category} em >> em.log
     done
 done

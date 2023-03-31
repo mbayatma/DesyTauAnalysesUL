1fa2ed68f2ac497e2ca0b403bd804c45ff73371e
Aliaksei Raspiareza <rasp@mail.desy.de>
2023-03-31 16:50:25 +0200

    clean merger for tt

--- BBHTT/test/dnn_production/dnn_merge_tt_2016.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# merges 2016_pre and 2016_post samples
-dir=/nfs/dust/cms/user/rasp/Run/tautau_dnn_newTauID_UL
-for sample in Tau DYJets_amcatnlo WJets_amcatnlo TTbar SingleTop Diboson GluGluHToTauTau VBFHToTauTau WHToTauTau ZHToTauTau TTHToTauTau BBHToTauTau_YT2 BBHToTauTau_YB2 BBHToTauTau_YBYT 
-do
-    rm ${dir}/2016/tt-NOMINAL_ntuple_${sample}_2016.root
-    hadd ${dir}/2016/tt-NOMINAL_ntuple_${sample}_2016.root ${dir}/2016_pre/tt-NOMINAL_ntuple_${sample}_2016_pre.root ${dir}/2016_post/tt-NOMINAL_ntuple_${sample}_2016_post.root
-done

449f96ebaeb9859af88523de839b2d1b7e4d1adf
Aliaksei Raspiareza <rasp@mail.desy.de>
2023-04-24 12:51:55 +0200

    Updated DNN merging scripts

--- BBHTT/test/dnn_production/dnn_producer_tt.bash
+++ BBHTT/test/dnn_production/dnn_producer_tt.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 # submits sequence of dnn producers 
-dir=/nfs/dust/cms/user/rasp/Run/jobs
+dir=/nfs/dust/cms/user/rasp/Run/log/dnn
 for era in 2016_pre 2016_post 2017 2018
 do
-    for sample in Tau DYJets_amcatnlo WJets_amcatnlo TTbar SingleTop Diboson GluGluHToTauTau VBFHToTauTau WHToTauTau ZHToTauTau TTHToTauTau BBHToTauTau_YT2 BBHToTauTau_YB2 BBHToTauTau_YBYT
+    for sample in Tau DYJets_amcatnlo WJets_amcatnlo TTbar SingleTop Diboson GluGluHToTauTau VBFHToTauTau WHToTauTau ZHToTauTau TTHToTauTau   BBHToTauTau_YT2 BBHToTauTau_YB2 BBHToTauTau_YBYT
     do
 	./dnn_producer.bash ${sample} ${era} tt ${dir}
     done

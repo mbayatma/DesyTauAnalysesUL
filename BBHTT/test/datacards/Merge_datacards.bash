c4e6131e3f809d7e411d3c1f34725dd63214e8e2
Aliaksei Raspiareza <rasp@mail.desy.de>
2023-04-24 12:53:19 +0200

    removed Merge_datacards.bash

--- BBHTT/test/datacards/Merge_datacards.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#dir=/nfs/dust/cms/user/rasp/Run/emu_datacards_sys_UL/BDT_coarse
-dir=/nfs/dust/cms/user/rasp/Run/tautau_datacards_sys_UL/BDT_coarse
-cd $dir
-for era in 2016 2017 2018
-do
-    rm 
-done
